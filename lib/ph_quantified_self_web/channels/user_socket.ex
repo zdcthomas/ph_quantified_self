c6d481ee1f6f90f1bfdb89dd8842d45590bb6035
zdcthomas <zdcthomas@yahoo.com>
2018-08-21 22:08:58 -0600

    alters production configuration of phoenix app

--- lib/ph_quantified_self_web/channels/user_socket.ex
+++ lib/ph_quantified_self_web/channels/user_socket.ex
@@ -7,6 +7,7 @@ defmodule PhQuantifiedSelfWeb.UserSocket do
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
   # transport :longpoll, Phoenix.Transports.LongPoll
+  timeout: 45_000
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
