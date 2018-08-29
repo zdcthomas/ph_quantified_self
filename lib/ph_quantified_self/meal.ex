33741488c31e1ab7ff99feb4f723abab4291446f
zdcthomas <zdcthomas@yahoo.com>
2018-08-28 21:24:42 -0600

    implemenets meal db queries

--- lib/ph_quantified_self/meal.ex
+++ lib/ph_quantified_self/meal.ex
@@ -3,6 +3,13 @@ defmodule PhQuantifiedSelf.Meal do
   import Ecto.Changeset
   import Ecto.Query, warn: false
   @derive {Poison.Encoder, except: [:__meta__]}
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  import Ecto.Query, warn: false
+  alias PhQuantifiedSelf.{Repo, Food, Meal}
+  alias PhQuantifiedSelf.Meal.Food, as: Meal_Food
+
 
 
 
@@ -17,5 +24,31 @@ defmodule PhQuantifiedSelf.Meal do
     meal
     |> cast(attrs, [:name])
     |> validate_required([:name])
+  end
+
+  def all do
+    Meal
+    |>select([:id, :name])
+    |>Repo.all()
+    |>Repo.preload(:foods)
+  end
+
+  def find(id) do
+    Meal
+    |>select([:id, :name])
+    |>Repo.get(id)
+    |>Repo.preload(:foods)
+  end
+
+  def add_food(meal, food) do
+    Repo.insert(%Meal_Food{meal_id: meal.id, food_id: food.id})
+    Meal.find(meal.id)
+  end
+
+  def remove_food(meal, food) do
+    Meal_Food
+    |>Repo.get_by(meal_id: meal.id, food_id: food.id)
+    |>Repo.delete()
+    {:ok, food, meal}
   end
 end
